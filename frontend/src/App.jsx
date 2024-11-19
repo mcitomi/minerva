5e5244c9f70dd947dbb84df21d0b46a6e2121381
GameFerret2006 <fulekijozsef.tanulas01@gmail.com>
2024-11-19 14:58:47 +0100

    imports from index.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home.jsx";
+import { Home, NotFound } from "./pages/index.js"
 
 export default () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home></Home>}></Route>
+                {/* Ide a többi oldalt */}
+                <Route path="*" element={<NotFound></NotFound>}></Route>
             </Routes>
         </Router>
     );
---
+++ frontend/src/pages/notfound.jsx
@@ -0,0 +1,3 @@
+export default () => {
+    return <h1>Az oldal nem található!</h1>;
+}
