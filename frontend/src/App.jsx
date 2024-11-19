af5b4a315e71385fd7c2d8e733377e2285b70231
GameFerret2006 <fulekijozsef.tanulas01@gmail.com>
2024-11-19 14:50:20 +0100

    react router import and usage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,13 @@
-export default function Main() {
-    return <h1>Main</h1>;
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home.jsx";
+
+export default () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+            </Routes>
+        </Router>
+    );
 }
