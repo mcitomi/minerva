c5e41a89700aac168bf476d785322bddbde656f5
GameFerret2006 <fulekijozsef.tanulas01@gmail.com>
2024-11-23 11:32:40 +0100

    create about.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, NotFound } from "./pages/index.js"
+import { Home, About, NotFound } from "./pages/index.js"
 
 export default () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home></Home>}></Route>
+                <Route path="/about" element={<About></About>}></Route>
                 {/* Ide a többi oldalt */}
                 <Route path="*" element={<NotFound></NotFound>}></Route>
             </Routes>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from "./home.jsx";
+export { default as About } from "./about.jsx";
 export { default as NotFound } from "./notfound.jsx";
