6b2cb23c7049ea1770b43f7830f080a87ae06318
GameFerret2006 <fulekijozsef.tanulas01@gmail.com>
2024-11-25 15:26:43 +0100

    create login.jsx and registration.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, About, NotFound } from "./pages/index.js"
+import { Home, About, Login, Registration ,NotFound } from "./pages/index.js"
 
 export default () => {
     return (
@@ -8,6 +8,8 @@ export default () => {
             <Routes>
                 <Route path="/" element={<Home></Home>}></Route>
                 <Route path="/about" element={<About></About>}></Route>
+                <Route path="/login" element={<Login></Login>}></Route>
+                <Route path="/registration" element={<Registration></Registration>}></Route>
                 {/* Ide a többi oldalt */}
                 <Route path="*" element={<NotFound></NotFound>}></Route>
             </Routes>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as Home } from "./home.jsx";
 export { default as About } from "./about.jsx";
+export { default as Login } from "./login.jsx";
+export { default as Registration } from "./registration.jsx";
 export { default as NotFound } from "./notfound.jsx";
