00a3c7ce028a81fda37c94f7bf34c02f01302265
Tomi <admin@mcitomi.hu>
2025-01-31 18:57:28 +0100

    styles folder + eslint

--- frontend/src/components/MainNavbar.jsx
+++ frontend/src/components/MainNavbar.jsx
@@ -2,34 +2,33 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom"
+import "../styles/nav.css";
 
 export default () => {
     return (
-        <Navbar expand="lg"  style={{backgroundColor: "#263557"}}>
+        <Navbar expand="lg" style={{ backgroundColor: "#263557" }}>
             <Container>
-                <div style={{display: "inline-block"}}>
-                <Navbar.Brand as={Link} to="/" style={{backgroundColor: "#ffbd59", borderTopRightRadius: 10, borderBottomRightRadius: 10}}>Főoldal</Navbar.Brand>
-               <div>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" ></Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <div>
-                            <Nav.Link as={Link} to="/about" style={{backgroundColor:"#d3eefd", borderTopRightRadius: 10, borderBottomRightRadius: 10}}>Rólunk</Nav.Link>
-                            <div>
-                                <Nav.Link as={Link} to="/login" style={{backgroundColor:"#a7d5fb", borderTopRightRadius: 10, borderBottomRightRadius: 10}} >Belépés</Nav.Link>
-                                 <div>
-                                    <Nav.Link as={Link} to="/registration" style={{backgroundColor:"#83b8e3", borderTopRightRadius: 10, borderBottomRightRadius: 10}}>Regisztráció</Nav.Link>
+                <div style={{ display: "inline-block" }}>
+                    <Navbar.Brand as={Link} to="/" style={{ backgroundColor: "#ffbd59", borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>Főoldal</Navbar.Brand>
+                    <div>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" ></Navbar.Toggle>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <div>
+                                    <Nav.Link as={Link} to="/about" style={{ backgroundColor: "#d3eefd", borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>Rólunk</Nav.Link>
                                     <div>
-                                         <Nav.Link as={Link} to="/shop" style={{backgroundColor:"#699fcb", borderTopRightRadius: 10, borderBottomRightRadius: 10}}>Vásárlás</Nav.Link>
+                                        <Nav.Link as={Link} to="/login" style={{ backgroundColor: "#a7d5fb", borderTopRightRadius: 10, borderBottomRightRadius: 10 }} >Belépés</Nav.Link>
+                                        <div>
+                                            <Nav.Link as={Link} to="/registration" style={{ backgroundColor: "#83b8e3", borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>Regisztráció</Nav.Link>
+                                            <div>
+                                                <Nav.Link as={Link} to="/shop" style={{ backgroundColor: "#699fcb", borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>Vásárlás</Nav.Link>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </Nav>
-                    
-                </Navbar.Collapse>
-                    
-                 </div>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </div>
                 </div>
             </Container>
         </Navbar>
