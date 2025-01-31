fc2fedf8dd3a8b7474c896770092af0a9b26f807
GameFerret2006 <fulekijozsef.tanulas01@gmail.com>
2025-01-31 15:45:34 +0100

    text uppercase

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,6 @@
-export { default as Home } from "./home.jsx";
-export { default as About } from "./about.jsx";
-export { default as Login } from "./login.jsx";
-export { default as Registration } from "./registration.jsx";
-export { default as Shop } from "./shop.jsx";
-export { default as NotFound } from "./notfound.jsx";
+export { default as Home } from "./Home.jsx";
+export { default as About } from "./About.jsx";
+export { default as Login } from "./Login.jsx";
+export { default as Registration } from "./Registration.jsx";
+export { default as Shop } from "./Shop.jsx";
+export { default as NotFound } from "./NotFound.jsx";
