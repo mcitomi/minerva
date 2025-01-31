d83b00c4dab8050faf312911b03e45018eba1915
GameFerret2006 <fulekijozsef.tanulas01@gmail.com>
2025-01-31 18:05:28 +0100

    home page with carousel

---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,20 @@
+import Carousel from "react-bootstrap/Carousel";
+
+export default () => {
+  return (
+    <Carousel>
+        <Carousel.Item>
+            <img src="" alt="Első kép" />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img src="" alt="Második kép" />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img src="" alt="Harmadik kép" />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img src="" alt="Negyedik kép" />
+        </Carousel.Item>
+    </Carousel>
+  );
+}
