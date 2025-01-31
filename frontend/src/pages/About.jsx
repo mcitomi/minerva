937225e9159f4b06cf1eb0f57f0f5f6d2934c7ad
GameFerret2006 <fulekijozsef.tanulas01@gmail.com>
2025-01-31 18:57:21 +0100

    login page :3

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import Carousel from "../components/Carousel";
 
 export default () => {
     return (
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,30 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+
 export default () => {
-    return <h1>Bejelentkezés</h1>;
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <img src="" alt="Dekor kép" />
+                </Col>
+                <Col>
+                    <h2>Bejelentkezés</h2>
+                    <Form>
+                        <FloatingLabel controlId="floatingInput" label="Email cím" className="mb-3">
+                            <Form.Control type="email" placeholder="Email cím"></Form.Control>
+                        </FloatingLabel>
+                        <FloatingLabel controlId="floatingPassword" label="Jelszó" className="mb-3">
+                            <Form.Control type="password" placeholder="Jelszó"></Form.Control>
+                        </FloatingLabel>
+                        <Button variant="warning">Belépés</Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
