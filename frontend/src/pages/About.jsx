7d3cf79ba55fac8166a93b752814db31f8409a6b
GameFerret2006 <fulekijozsef.tanulas01@gmail.com>
2025-01-31 18:26:30 +0100

    about page :3

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,3 +1,58 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Carousel from "../components/Carousel";
+
 export default () => {
-    return <h1>Rólunk</h1>;
+    return (
+        <Container>
+            <h2>Készítők:</h2>
+            <Row>
+                <Col>
+                    <img src="" alt="Berdó kép" />
+                </Col>
+                <Col>
+                    <h3>Berdó Tamás</h3>
+                    <p>Tomi kiváló backend programozó, akinek a weboldal készítés a kisujjában van. Modern technikákkal dolgozik, mindig naprakész.</p>
+                    <p>Imádja a macskákat és a programozást. Ő egy igazi „Code Cat”. :3</p>
+                    <p>Cicafiú 19 éves, programozó.</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h3>Füleki József</h3>
+                    <p>Józsi jó frontend programozó és kiváló rajzoló. Modern technikákkal dolgozik, mindig naprakész.</p>
+                    <p>Imádja az animációkat. Távol áll tőle az informatika bonyolult világa, de a szíve lelkét beleadja.</p>
+                    <p>Jósk 18 éves, rajzoló.</p>
+                </Col>
+                <Col>
+                    <img src="" alt="Füleki kép" />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <img src="" alt="Sartner kép" />
+                </Col>
+                <Col>
+                    <h3>Sartner Bettina</h3>
+                    <p>Betti kiváló fotós, akinek a fényképezés a kisujjában van. Modern technikákkal dolgozik, mindig naprakész.</p>
+                    <p>Imádja a kutyákat és a fényképezést. A programozás bár nem erőssége, de a szíve lelkét beleadja.</p>
+                    <p>18 éves, fényképész.</p>
+                </Col>
+            </Row>
+            <h2>Konzulens:</h2>
+            <Row>
+                <Col>
+                    <h3>Molnár Máté Norbert</h3>
+                    <p>Komédiás 1 kiváló programozó és vicces tanár. Modern technikákkal dolgozik, mindig naprakész.</p>
+                    <p>Imádja a programozást és a focit. Fontos számára, hogy diákjaival meg tudja szerettetni az informatikát és minél többen a szakmájukban dolgozzanak.</p>
+                    <p>Máré 21 éves, tanár.</p>
+                </Col>
+                <Col>
+                    <img src="" alt="Molnár kép" />
+                </Col>
+            </Row>
+        </Container>
+    );
 }
