f0a132a07d304d12fb28675804f393217bd23177
GameFerret2006 <fulekijozsef.tanulas01@gmail.com>
2025-02-01 10:15:41 +0100

    notfound page :3

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,3 +1,13 @@
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom"
+
 export default () => {
-    return <h1>Az oldal nem található!</h1>;
+    return (
+        <Container>
+            <h1>Upszi! Valami hiba történt!</h1>
+            <p>Keresésed során rossz végpontot adtál meg. Térj vissza a főoldalra!</p>
+            <Button variant="warning" as={Link} to="/">Főoldal</Button>
+        </Container>
+    );
 }
