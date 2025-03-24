610556743cdf5765227f948670fdcb1f773edccb
Tomi <admin@mcitomi.hu>
2025-03-24 22:43:24 +0100

    Image resizer, request optimalization

--- backend/src/routes/post/forum/profiles.ts
+++ backend/src/routes/post/forum/profiles.ts
@@ -1,4 +1,6 @@
 import { Database } from "bun:sqlite";
+import sharp from "sharp";
+
 import { verifyToken } from "../../../modules/auth/jwt";
 import { type Payload } from "../../../types/jwt";
 
@@ -43,14 +45,14 @@ export const handleRequest = async (req: Request, db: Database) => {
             }, { status: 400 })
         }
 
-        const placeholders = body.profileIds.map(() => '?').join(',');
+        const placeholders = body.profileIds.map(() => '?').join(',');  // annyi ?-et illeszt a lekérdezésbe, ahány adatot kapunk frontendről.
 
         const query = db.query(`
             SELECT credentials.id AS userId, pictureBase64Url, isActive, username FROM credentials
             LEFT JOIN profileDetails ON profileDetails.credentialsId = credentials.id
             WHERE credentials.id IN (${placeholders});`
         );
-        
+
         const rawUsers = query.all(...body.profileIds) as {
             isActive: number;
             userId: number;
@@ -58,16 +60,47 @@ export const handleRequest = async (req: Request, db: Database) => {
             username: string;
         }[];
 
-        return Response.json(rawUsers.map(userObject => {
-            return {
-                userId : userObject.isActive == 1 ? userObject.userId : null,
-                username: userObject.isActive == 1 ? decryptRSA(userObject.username) : null,
-                pictureBase64Url: userObject.isActive == 1 && userObject.pictureBase64Url ? userObject.pictureBase64Url : null
-            }
-        }));
+        // kis kérés optimalizálás: minden képet 128x128as felbontásban küldünk ki mint a dc.
+        return Response.json(
+            await Promise.all(  // a Promise.all megvár minden promise-t a blokkon belül, mert a .map nem várja meg az await dolgokat valamiért 
+                rawUsers.map(async userObject => {
+                    if (userObject.isActive == 1 && userObject.pictureBase64Url) {
+                        try {
+                            const imageBuffer = Buffer.from(userObject.pictureBase64Url.split(",")[1], "base64");
+
+                            // const imageFormatFromBase64MIME = userObject.pictureBase64Url.split(";")[0].split("/")[1];
+
+                            const resizedPictureBase64 = (await sharp(imageBuffer, { animated: true })
+                                .resize(128, 128)
+                                .toFormat("gif")    // minden képet gif-ben konvertálunk, mert az mindent tud kezelni
+                                .toBuffer())
+                                .toString("base64");
+
+                            return {
+                                userId: userObject.userId,
+                                username: decryptRSA(userObject.username),
+                                pictureBase64Url: "data:image/gif;base64," + resizedPictureBase64   
+                                // a base64 headert manuálisan visszarakjuk, mert valamiért a sharp nem ismeri fel magától a mime formátumot
+                            };
+
+                        } catch (e) {
+                            return {
+                                userId: userObject.userId,
+                                username: decryptRSA(userObject.username),
+                                pictureBase64Url: userObject.pictureBase64Url
+                            };
+                        }
+                    }
+
+                    return {
+                        userId: userObject.isActive == 1 ? userObject.userId : null,
+                        username: userObject.isActive == 1 ? decryptRSA(userObject.username) : null,
+                        pictureBase64Url: null
+                    };
+                })
+            )
+        );
     } catch (error) {
-        console.log(error);
-        
         if (error.message.includes("token")) {
             return Response.json({
                 "message": [error.message]
